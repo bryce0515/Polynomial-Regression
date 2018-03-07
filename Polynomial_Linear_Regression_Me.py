1b6a17cd78fed55efe873e8367c25b3b55c6cbd4
bryce0515 <bryce0515@gmail.com>
2018-03-07 12:44:57 -0500

    Polynomial Regression
    A model fitting the data to a Polynomial Regression using nnumpy, and pandas.

---
+++ Polynomial_Linear_Regression_Me.py
@@ -0,0 +1,65 @@
+# Polynomial Regression
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Position_Salaries.csv')
+# Make x a vector so the index below makes 1 column
+X = dataset.iloc[:, 1:2].values
+y = dataset.iloc[:, 2].values
+
+# Splitting the dataset into the Training set and Test set
+"""from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+"""
+# Feature Scaling
+"""from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+sc_y = StandardScaler()
+y_train = sc_y.fit_transform(y_train)"""
+
+# Fitting Linear Regression to the dataset
+from sklearn.linear_model import LinearRegression
+lin_reg = LinearRegression()
+lin_reg.fit(X, y)
+
+# Fitting Polynomial Regression to the dataset
+from sklearn.preprocessing import PolynomialFeatures
+# create a matrix with a column of 1's, the levels, and a row of increasing powers
+poly_reg = PolynomialFeatures(degree = 4)
+X_poly = poly_reg.fit_transform(X)
+lin_reg_2 = LinearRegression()
+lin_reg_2.fit(X_poly, y)
+
+# Visualising the Linear Regression results
+plt.scatter(X, y, color = 'red')
+plt.plot(X, lin_reg.predict(X), color = 'blue')
+plt.title('Truth or Bluff (Linear Regression)')
+plt.xlabel('Position Level')
+plt.ylabel('Salary')
+plt.show
+         
+# Visualising the Polynomial Regression results
+# X_grid will create a higher resolution by increasing the steps in between the 10 levels
+X_grid = np.arange(min(X), max(X), 0.001)
+X_grid = X_grid.reshape((len(X_grid), 1))
+plt.scatter(X, y, color = 'red')
+plt.plot(X, lin_reg_2.predict(poly_reg.fit_transform(X)), color = 'blue')
+plt.title('Truth or Bluff (Linear Regression)')
+plt.xlabel('Position Level')
+plt.ylabel('Salary')
+plt.show
+
+# Predicting a new result with Linear Regression
+# Find the salary from the linear model that correlates to 6.5 hours
+lin_reg.predict(6.5)
+
+# Predicting a new result with Polynomial Regression
+# Find Salary from the Poly model that correlates to 6.5 hours
+lin_reg_2.predict(poly_reg.fit_transform(6.5))
+
